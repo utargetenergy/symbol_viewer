2d0da9e2e323a2e8eb108d1525ff5bc2f6f6f7df
Mark Chen <mchen@uts-canada.com>
2019-07-30 17:33:44 -0600

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@
 
 module.exports = function (grunt) {
 
+  grunt.loadNpmTasks('grunt-build-control');
+
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
