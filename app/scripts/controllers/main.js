79f781a8c740fdbbad2807ddb5b419baa7bd4f5a
Mark Chen <mchen@uts-canada.com>
2019-08-01 10:52:34 -0600

    added IE detection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,4 +20,22 @@ angular.module('symbolViewerAppApp')
         return viewLocation === $location.path();
     };
 
+    $scope.ie = getIEVersion(); 
+
+    function getIEVersion() {
+      var sAgent = window.navigator.userAgent;
+      var Idx = sAgent.indexOf("MSIE");
+
+      // If IE, return version number.
+      if (Idx > 0) 
+        return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+      // If IE 11 then look for Updated user agent string.
+      else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+        return 11;
+
+      else
+        return 0; //It is not IE
+    }
+    
   });
